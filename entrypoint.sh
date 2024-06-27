a87570e6a9359bdc4b3d5daaac65aab785cb5474
Car85 <car85@startmail.com>
2024-06-27 04:16:00 -0700

    Update entrypoint.sh
    first changes in entrypoint.sh to runs chronos under gunicorn 22.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+set -e  
+
 socat -d -d PTY,link=/tmp/ptyp0,raw,echo=0 PTY,link=/tmp/ttyp0,raw,echo=0 &
 socat -d -d PTY,link=/tmp/ptyp1,raw,echo=0 PTY,link=/tmp/ttyp1,raw,echo=0 &
-python2 working-sync-server.py /tmp/ttyp0 &
-sudo chown pi:pi /tmp/ptyp0
-sudo chown pi:pi /tmp/ptyp1
-sudo chown -R pi:pi /home/pi
-sleep 2
-mkdir /var/log/uwsgi
-/usr/local/bin/uwsgi --ini /etc/uwsgi/apps-enabled/socketio_server.ini --pidfile /var/run/uwsgi/uwsgi-socketio.pid --daemonize /var/log/uwsgi/uwsgi-socketio.log
+python3 working-sync-server.py /tmp/ttyp0 &  # Usar python3 en lugar de python2
 
-# Added to simulate water temperature in and out
+chown pi:pi /tmp/ptyp0
+chown pi:pi /tmp/ptyp1
+chown -R pi:pi /home/pi
+
+sleep 2
+mkdir -p /var/log/gunicorn
+
+echo "Runing Gunicorn"
+gunicorn --workers 3 --bind 0.0.0.0:5000 wsgi:app &
+
 echo -e "YES\nt=100" > /tmp/water_in
 echo -e "YES\nt=150" > /tmp/water_out
 
-service uwsgi start
 service nginx start
 service chronos start
+
 tail -f /dev/null
